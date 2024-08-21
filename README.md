91c2471f492af9d3e9499b181caec497e3d27005
iNimrod <brodlefleur@gmail.com>
2024-08-21 12:48:16 +0800

    first working version

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # comfyui-launcher
- GNU/Linux (Ubuntu 22.04) installer and launcher for ComfyUI for Stable Diffusion
+ GNU/Linux (Ubuntu 22.04) installer and launcher for [ComfyUI](https://github.com/comfyanonymous/ComfyUI).
+
+ Can be used for running inferences on [Flux.1](https://blackforestlabs.ai/#get-flux) models.
+
+
+## Installing:
+1. Install `TCMalloc` if not yet installed: `sudo apt-get install -y libgoogle-perftools4 libtcmalloc-minimal4`
+1. Clone this repo: `git clone https://github.com/inimrod/comfyui-launcher.git comfyui && cd comfyui && chmod +x run.sh`.
+1. Run for the first time: `./run.sh`
+
+# Manual Usage
+1. Run: `./run.sh`.
---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+##### SET FOLLOWING VARS #####
+SERVER_ADDR="0.0.0.0"
+SERVER_PORT=7868
+CLONE_DIR="comfyui"
+VENV_DIR="venv"
+###### END OF VARIABLES ######
+
+# set working dir to this script's location:
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd $SCRIPT_DIR
+
+# check if install dir already exists
+if [ ! -d "$CLONE_DIR" ]; then
+    echo "Cloning ComfyUI repo..."
+    echo ""
+    git clone https://github.com/comfyanonymous/ComfyUI.git $CLONE_DIR
+fi
+
+cd $CLONE_DIR
+if [ ! -d "$VENV_DIR" ]; then
+    echo ""
+    echo "Setting up python virtual environment..."
+    echo ""
+    python3 -m venv venv
+    source "${VENV_DIR}"/bin/activate
+    "${VENV_DIR}"/bin/python -m pip install --upgrade pip
+    "${VENV_DIR}"/bin/python -m pip install -r requirements.txt
+else
+    source "${VENV_DIR}"/bin/activate
+fi
+
+$VENV_DIR/bin/python main.py --listen $SERVER_ADDR --port $SERVER_PORT --multi-user
